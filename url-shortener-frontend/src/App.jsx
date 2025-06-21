36b3aa910217da3ae36f833bcfd7b6d82eb2309e
Ashwani-yadav01 <yadavashwaniyadav273209@gmail.com>
2025-06-21 20:03:47 +0530

    Created Dashboard and ending the day (21 June)

--- url-shortener-frontend/src/App.jsx
+++ url-shortener-frontend/src/App.jsx
@@ -7,6 +7,8 @@ import RegisterPage from "./component/RegisterPage";
 import Navbar from "./component/Navbar";
 import Footer from "./component/Footer";
 import { Toaster } from "react-hot-toast";
+import LoginPage from "./component/LoginPage";
+import DashboardLayout from "./component/Dashboard/DashboardLayout";
 
 
 function App() {
@@ -19,7 +21,9 @@ function App() {
           <Route path="/" element={<LandingPage />} />
           <Route path="/about" element={<AboutPage />} />
           <Route path="/register" element={<RegisterPage />} />
-          
+          <Route path="/login" element={<LoginPage/>}/>
+          <Route path="/dashboard" element={<DashboardLayout/>}/>
+
         </Routes>
         <Footer/>
       </BrowserRouter>
