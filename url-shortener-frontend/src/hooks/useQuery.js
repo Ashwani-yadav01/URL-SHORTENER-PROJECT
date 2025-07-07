43685f321f72850837c9f66ae793629236668547
Ashwani-yadav01 <yadavashwaniyadav273209@gmail.com>
2025-07-07 16:19:05 +0530

    refech function added

--- url-shortener-frontend/src/hooks/useQuery.js
+++ url-shortener-frontend/src/hooks/useQuery.js
@@ -32,7 +32,7 @@ export const useFetchTotalClicks = (token, onError) => {
     "url-totalclick",
     async () => {
       return await api.get(
-        "/api/urls/totalClicks?startDate=2024-01-01&endDate=${today}",
+        `/api/urls/totalClicks?startDate=2024-01-01&endDate=${today}`,
         {
           headers: {
             "Content-Type": "application/json",
